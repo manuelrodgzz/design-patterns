951a5cf625b4a2d270e418a34723326bbe5f126a
Manuel Alejandro Rodríguez González <mrodriguez@MRODRIGUEZ2-MAC.local>
2024-01-03 06:47:33 -0600

    feat: Create proxy example

---
+++ structural/proxy/README.md
@@ -0,0 +1,28 @@
+# Proxy
+
+## Intent
+Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object. A proxy controls access to the original object, allowing you to perform something either before or after the request gets through to the original object.
+
+## Applicability
+There are dozens of ways to utilize the Proxy pattern. Let’s go over the most popular uses.
+
+- 🐞 Lazy initialization (virtual proxy). This is when you have a heavyweight service object that wastes system resources by being always up, even though you only need it from time to time.
+- ⚡️ Instead of creating the object when the app launches, you can delay the object’s initialization to a time when it’s really needed.
+---
+- 🐞 Access control (protection proxy). This is when you want only specific clients to be able to use the service object; for instance, when your objects are crucial parts of an operating system and clients are various launched applications (including malicious ones).
+- ⚡️ The proxy can pass the request to the service object only if the client’s credentials match some criteria.
+---
+- 🐞 Local execution of a remote service (remote proxy). This is when the service object is located on a remote server.
+- ⚡️ In this case, the proxy passes the client request over the network, handling all of the nasty details of working with the network.
+---
+- 🐞 Logging requests (logging proxy). This is when you want to keep a history of requests to the service object.
+- ⚡️ The proxy can log each request before passing it to the service.
+---
+- 🐞 Caching request results (caching proxy). This is when you need to cache results of client requests and manage the life cycle of this cache, especially if results are quite large.
+- ⚡️ The proxy can implement caching for recurring requests that always yield the same results. The proxy may use the parameters of requests as the cache keys.
+---
+- 🐞 Smart reference. This is when you need to be able to dismiss a heavyweight object once there are no clients that use it.
+- ⚡️ The proxy can keep track of clients that obtained a reference to the service object or its results. From time to time, the proxy may go over the clients and check whether they are still active. If the client list gets empty, the proxy might dismiss the service object and free the underlying system resources.
+The proxy can also track whether the client had modified the service object. Then the unchanged objects may be reused by other clients.
+
+[More info](https://refactoring.guru/design-patterns/proxy)
---
+++ structural/proxy/index.ts
@@ -0,0 +1,127 @@
+interface ThirdPartyYouTubeLib {
+  listVideos(): Promise<string[]>
+  getVideoInfo(id: string): Promise<string>
+  downloadVideo(id: string): Promise<any>
+}
+
+class ThirdPartyYouTubeClass implements ThirdPartyYouTubeLib {
+
+  constructor() {}
+
+  listVideos(): Promise<string[]> {
+    return new Promise((res) => {
+      setTimeout(() => res(['video1', 'video2', 'video3']), 800)
+    })
+  }
+
+  getVideoInfo(id: string): Promise<string> {
+    return new Promise((res) => {
+      setTimeout(() => res(`Description for video ${id}`), 200)
+    })
+  }
+
+  downloadVideo(id: string): Promise<any> {
+    return new Promise((res) => {
+      setTimeout(() => res({}), 1000)
+    })
+  }
+}
+
+class CachedYouTubeClass implements ThirdPartyYouTubeLib {
+  service: ThirdPartyYouTubeLib
+  videosListCache: string[] = []
+  videoInfoCache: Record<string, string> = {}
+  videoCache: Record<string, {}> = {}
+  lastCache: number = 0
+
+  constructor(service: ThirdPartyYouTubeLib) {
+    this.service = service
+  }
+
+  private shouldUpdateCache(): boolean {
+    const now = new Date().getTime()
+
+    // udpate cache every 30 seconds
+    if (now - this.lastCache > 30_000) {
+      this.lastCache = now
+      return true
+    }
+
+    return false
+  }
+
+  async listVideos(): Promise<string[]> {
+    const cacheUpdateRequired = this.shouldUpdateCache()
+    if (!this.videosListCache.length || cacheUpdateRequired) {
+      const freshVideosList = await this.service.listVideos()
+
+      this.videosListCache.push(
+        ...freshVideosList  
+      )
+    }
+
+    return this.videosListCache
+  }
+
+  async getVideoInfo(id: string): Promise<string> {
+    const cacheUpdateRequired = this.shouldUpdateCache()
+    if (!this.videoInfoCache[id] || cacheUpdateRequired) {
+      const newVideoInfo = await this.service.getVideoInfo(id)
+      this.videoInfoCache[id] = newVideoInfo
+    }
+
+    return this.videoInfoCache[id]
+  }
+
+  async downloadVideo(id: string): Promise<any> {
+    const cacheUpdateRequired = this.shouldUpdateCache()
+    if (!this.videoCache[id] || cacheUpdateRequired) {
+      const video = await this.service.downloadVideo(id)
+      this.videoCache[id] = video
+    }
+
+    return this.videoCache[id]
+  }
+}
+
+class YouTuberManager {
+  service: ThirdPartyYouTubeLib
+
+  constructor(service: ThirdPartyYouTubeLib) {
+    this.service = service
+  }
+
+  async renderVideo(id: string) {
+    const timeLabel = `renderVideo ${id}`
+    console.time(timeLabel)
+    await this.service.getVideoInfo(id)
+    await this.service.downloadVideo(id)
+    console.timeEnd(timeLabel)
+    // render video page
+  }
+
+  async renderListPanel() {
+    const timeLabel = `renderListPanel`
+    console.time(timeLabel)
+    await this.service.listVideos()
+    console.timeEnd(timeLabel)
+  }
+}
+
+async function clientCode() {
+  const youtubeService = new ThirdPartyYouTubeClass()
+  const youtubeProxy = new CachedYouTubeClass(youtubeService)
+  const manager = new YouTuberManager(youtubeProxy)
+
+  console.log('--- Rendering list panel for the first time ---')
+  await manager.renderListPanel()
+  console.log('--- Rendering video for the first time ---')
+  await manager.renderVideo('asd')
+
+  console.log('--- Rendering list panel for the second time ---')
+  await manager.renderListPanel()
+  console.log('--- Rendering video for the second time ---')
+  await manager.renderVideo('asd')
+}
+
+clientCode()
